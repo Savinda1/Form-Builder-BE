a6f1ac7ae2aa81792d03d09bae225de229412365
Amila <155749022+Savinda1@users.noreply.github.com>
2025-09-21 19:35:25 +0530

    link

--- src/index.ts
+++ src/index.ts
@@ -23,9 +23,9 @@ conectDB();
 
 // Middleware
 
-app.use(cors());
+//app.use(cors());
 
-//app.use(cors({ origin: process.env.FRONTEND_URL }));
+app.use(cors({ origin: process.env.FRONTEND_URL }));
 
 /*app.post(
   "/api/stripe/webhook",
